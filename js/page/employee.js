74389cad7e7ce3e14e01eccb313b54a744a04eca
PetterPhuc <phuclk@tandan.com.vn>
2021-07-17 23:01:26 +0700

    version 1.0.2 fix UI after review 17/7/20201

--- js/page/employee.js
+++ js/page/employee.js
@@ -1,5 +1,47 @@
 $(document).ready(function(){
-    $('#add-employee-btn').click(function(){
-        $('#detail-employee').css('display', 'block')
+    $("#add-employee-btn").click(function(){
+        $("#detail-employee").css("display", "block")
     })
+
+
+    // leaning with advanced js
+    // var person = {
+    //     firstName : "Le",
+    //     lastName : "Phuc",
+    //     age : 22,
+    //     address : "Hoa Phu - Ung Hoa - HN"
+    // }
+
+    // var phuc = new Object()
+    // phuc.firstName = "Le"
+    // phuc.lastName = "Phuc"
+    // phuc.age = 22
+    // phuc.address = "Hoa Phu - Ung Hoa - HN"
+
+    
+
+    // function Person(firstName, lastName, age, address){
+    //     this.firstName = firstName
+    //     this.lastName = lastName
+    //     this.address = address
+    //     this.age = age
+    // }
+
+    // var phuong = new Person("le", "phuong", 24, "Hoa Phu - Ung Hoa - HN")
+    // alert(person["firstName"] + "-" + phuong.lastName)
+
+    // var phuc = function(arr){
+    //     var mes = ""
+    //     arr.forEach(element => {
+    //         mes += element + "-"
+    //     });
+    //     return mes
+    // } 
+    
+    // alert(phuc([1, 2, 3]))
+
+
+
+    // end
+
 })
